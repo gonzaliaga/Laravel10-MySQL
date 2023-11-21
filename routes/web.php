2685be33ce3cb465ffcd6114a526ae0e7df351d6
gonzaliaga <gonzaliaga@gmail.com>
2023-11-21 17:59:46 -0300

    nuevo site en Laaravel 10 + SQL + Vite

--- routes/web.php
+++ routes/web.php
@@ -2,30 +2,32 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Models\Chirp;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+Route::view('/', 'welcome')->name('welcome');
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+
+
 
 Route::middleware('auth')->group(function () {
+    Route::view('/dashboard','dashboard')->name('dashboard');
+    
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::get('/chirps', function () {
+        return view('chirps.index');
+    })->name('chirps.index');
+    Route::post('/chirps', function () {
+        // insert into database
+        Chirp::create([
+            'user_id' => auth()->id(),
+            'message' => request('message'),
+        ]);
+        // se puede resumir esta linea session()->flash('status', 'Chirp created!');
+        return to_route('chirps.index')->with('status', __('Chirp created successfully!')); // poniedola aca, se puede usar en cualquier vista
+    });
 });
 
 require __DIR__.'/auth.php';
